2ad13a71d257dd1b13ef30dbc13bcc4dd8fc9ec4
gemechuj <gemechujimacs@gmail.com>
2024-02-26 15:12:57 +0300

    ee

--- src/Components/Backdrop.jsx
+++ src/Components/Backdrop.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 
 function Backdrop({onClick}) {
   return ReactDOM.createPortal(
-    <div className='w-screen h-screen bg-black opacity-40 fixed z-30' onClick={onClick}>
+    <div className='w-screen h-screen bg-black opacity-50 fixed z-30' onClick={onClick}>
     </div>, 
      document.getElementById('backdrop-hook')
   )
