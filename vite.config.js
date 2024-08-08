dd33fd8a814f5c7e6d7be974ebba3a735266ed4b
gemechuj <gemechujimacs@gmail.com>
2024-08-08 09:48:07 +0300

    update site deploy

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000/api',
+        target: 'https://portfolio-fevu.onrender.com/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
